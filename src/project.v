5132b4f15a810bf85d48ad22883efba36ad8cb6d
Lucaz97 <lc4976@nyu.edu>
2024-03-28 13:58:53 -0400

    changed top name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_lucaz97_monobit (
+module tt_um_lucaz97_rng_tests (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
